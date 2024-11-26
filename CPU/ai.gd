a0b2836494799540e782aa1615b653ecd88f824f
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2024-11-26 18:48:05 +0100

    AI migliorata

--- CPU/ai.gd
+++ CPU/ai.gd
@@ -3,8 +3,8 @@ extends Node
 
 #!!!IA per tesrtare le funzinalità!!!!
 
-@export var enable: bool = false
-@export var FireRate: float = 2
+@export var enable: bool = false  #abilita l'autoplay 
+@export var FireRate: float = 2 #ogni quanto indovina un'espressione
 
 @onready var timer: Timer = $Timer
 
@@ -13,20 +13,21 @@ extends Node
 
 @onready var cavalieri: Node = $"../GeneratoreDiCavalieri/Cavalieri"
 
-var can_shoot:bool = true
+
 func _ready() -> void:
 	if enable:
 		timer.wait_time = FireRate
 		timer.timeout.connect(_on_timer_timeout)
-
+	else:
+		queue_free()
+	
 
 func _on_timer_timeout() -> void:
-	can_shoot = true
 	fire()
 	
 	
 func fire():
-	if cavalieri.get_child_count() > 0 && can_shoot:
+	if cavalieri.get_child_count() > 0 :
 		input.text = str(cavalieri.get_child(0).risultato)
-		can_shoot = false
+
 	
